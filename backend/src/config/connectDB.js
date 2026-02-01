5d42d3e7c2434824b846a202845221c87c2026e6
Md Danish Alam <rankersacademy0@gmail.com>
2026-02-01 21:14:33 +0530

    [backend] conflict resolved

--- backend/src/config/connectDB.js
+++ backend/src/config/connectDB.js
@@ -2,14 +2,12 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI+"/Loan-Utilization-System");
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('Database connection failed:', err.message);
     process.exit(1); // Exit if connection fails
   }
 };
-
-export default connectDB
 
 export default connectDB;
